6e7d62833c313dcce1f9bae5f45aadff3b77ce16
Charlie Sanders <sanderscharlie@gmail.com>
2013-06-02 13:26:07 -0500

    Update comments for low level listen example

--- code/echo/listen_low.rb
+++ code/echo/listen_low.rb
@@ -2,10 +2,10 @@
 
 require 'socket'
 
-# Create a TCP socket
+# Create an IPv4 (:INET) TCP (:STREAM) socket.
 socket = Socket.new(:INET, :STREAM)
 
-# Create a C sturct to hold the address for listening
+# Create a C struct to hold the address for listening
 addr = Socket.pack_sockaddr_in(31337, '0.0.0.0')
 
 socket.bind(addr)
