f04af5f41a0c9f1078a306b826f9ff1c708b61b0
Charlie Sanders <sanderscharlie@gmail.com>
2013-06-03 23:03:43 -0500

    Use long form of socket constants for lowest level example

--- code/echo/listen_low.rb
+++ code/echo/listen_low.rb
@@ -2,8 +2,8 @@
 
 require 'socket'
 
-# Create an IPv4 (:INET) TCP (:STREAM) socket.
-socket = Socket.new(:INET, :STREAM)
+# Create a TCP socket for an IPv4 address
+socket = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
 
 # Create a C struct to hold the address for listening
 addr = Socket.pack_sockaddr_in(31337, '0.0.0.0')
